0321e99d6a2be6e4f376f2e773ea005504765dde
CundullahT <cundullahterzioglu.it@gmail.com>
2021-02-20 22:10:48 +0100

    Initial Commit

--- src/main/java/com/cybertek/controller/LoginController.java
+++ src/main/java/com/cybertek/controller/LoginController.java
@@ -19,7 +19,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
