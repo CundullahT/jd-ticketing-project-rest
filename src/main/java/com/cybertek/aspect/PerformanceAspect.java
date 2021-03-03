11b92c03631c3f29bb03f7fa688787afdacf0477
CundullahT <cundullahterzioglu.it@gmail.com>
2021-03-03 03:46:28 +0100

    Initial Commit

--- src/main/java/com/cybertek/aspect/PerformanceAspect.java
+++ src/main/java/com/cybertek/aspect/PerformanceAspect.java
@@ -1,14 +1,41 @@
 package com.cybertek.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+
 
 @Aspect
 @Configuration
 public class PerformanceAspect {
 
-    @Pointcut("execution()")
-    private void x(){}
+    Logger log = LoggerFactory.getLogger(PerformanceAspect.class);
+
+    @Pointcut("@annotation(com.cybertek.annotation.ExecutionTime)")
+    private void anyExecutionTimeOperation(){}
+
+    @Around("anyExecutionTimeOperation()")
+    public Object anyExecutionTimeOperationAdvice(ProceedingJoinPoint proceedingJoinPoint){
+
+        long beforeTime = System.currentTimeMillis();
+        Object result = null;
+
+        try {
+            result = proceedingJoinPoint.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+        long afterTime = System.currentTimeMillis();
+
+        log.info("Time taken to execute : {} ms (Method: {} - Parameters: {})",(afterTime - beforeTime), proceedingJoinPoint.getSignature().toShortString(), proceedingJoinPoint.getArgs());
+
+        return result;
+
+    }
 
 }
