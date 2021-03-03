a4b8eb44843bfbc2d31f5e829049f2e0912bf967
CundullahT <cundullahterzioglu.it@gmail.com>
2021-03-03 04:56:55 +0100

    Initial Commit

--- src/main/java/com/cybertek/aspect/LoggingAspect.java
+++ src/main/java/com/cybertek/aspect/LoggingAspect.java
@@ -3,9 +3,6 @@ package com.cybertek.aspect;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.hibernate.mapping.Join;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,31 +12,26 @@ import org.springframework.security.core.context.SecurityContextHolder;
 @Slf4j
 public class LoggingAspect {
 
-
     @Pointcut("execution(* com.cybertek.controller.ProjectController.*(..)) || execution(* com.cybertek.controller.TaskController.*(..))")
-    private void anyControllerOperation(){}
+    private void anyControllerOperation() {
+    }
 
     @Before("anyControllerOperation()")
-    public void anyBeforeControllerOperationAdvice(JoinPoint joinPoint){
+    public void anyBeforeControllerOperationAdvice(JoinPoint joinPoint) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        log.info("Before(User : {} Method : {} - Parameters : {}",auth.getName(),joinPoint.getSignature().toShortString(),joinPoint.getArgs());
+        log.info("Before(User : {} Method : {} - Parameters : {}", auth.getName(), joinPoint.getSignature().toShortString(), joinPoint.getArgs());
     }
 
-    @AfterReturning(pointcut = "anyControllerOperation()",returning = "results")
-    public void anyAfterReturningControllerOperationAdvice(JoinPoint joinPoint,Object results){
+    @AfterReturning(pointcut = "anyControllerOperation()", returning = "results")
+    public void anyAfterReturningControllerOperationAdvice(JoinPoint joinPoint, Object results) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        log.info("AfterReturning(User : {} Method : {} - Results : {}",auth.getName(),joinPoint.getSignature().toShortString(),results.toString());
+        log.info("AfterReturning(User : {} Method : {} - Results : {}", auth.getName(), joinPoint.getSignature().toShortString(), results.toString());
     }
 
-    @AfterThrowing(pointcut = "anyControllerOperation()",throwing = "exception")
-    public void anyAfterThrowingControllerOperationAdvice(JoinPoint joinPoint,RuntimeException exception){
+    @AfterThrowing(pointcut = "anyControllerOperation()", throwing = "exception")
+    public void anyAfterThrowingControllerOperationAdvice(JoinPoint joinPoint, RuntimeException exception) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        log.info("After throwing(User : {} Method : {} - Exception : {}",auth.getName(),joinPoint.getSignature().toShortString(),exception.getLocalizedMessage());
+        log.info("After throwing(User : {} Method : {} - Exception : {}", auth.getName(), joinPoint.getSignature().toShortString(), exception.getLocalizedMessage());
     }
-
-
-
-
-
 
 }
